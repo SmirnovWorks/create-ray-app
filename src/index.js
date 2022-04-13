f6ad4947db718451e4f80fc1dc7ed4eb2d1aa238
Alexey Smirnov <alexey.smirnov@wework.com>
2022-04-13 17:14:31 +0300

    add ability to link and instructions how to run

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "@wework/ray2/dist/components.css";
 
 ReactDOM.render(
   <React.StrictMode>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,7 @@ const { tailwindPreset } = require("@wework/ray2");
 
 module.exports = {
   content: [
-    './src/**/*.{html,js,jsx,ts,tsx}',
-    './node_modules/@wework/ray2/**/*.{html,js,jsx,ts,tsx}'
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   presets: [tailwindPreset]
 };
